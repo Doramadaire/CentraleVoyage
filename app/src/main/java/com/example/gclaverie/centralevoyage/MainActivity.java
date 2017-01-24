7feec932a2176da5b8cb2b6e643bbe48d7a2bd56
Claverie Guilhem <gclaverie@calade161.etu.ec-m.fr>
2017-01-24 19:30:37 +0100

    tâche asynchrone et progressBar fonctionnelles

--- app/src/main/java/com/example/gclaverie/centralevoyage/MainActivity.java
+++ app/src/main/java/com/example/gclaverie/centralevoyage/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.gclaverie.centralevoyage;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +20,7 @@ import org.json.JSONObject;
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    protected ProgressBar pBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +32,9 @@ public class MainActivity extends Activity {
         parameters.put("lon", "6.00246207789145");
         parameters.put("offset", "0");
         final String apiUrl = "http://voyage2.corellis.eu/api/v2/homev2?";
+
+        pBar = (ProgressBar) findViewById(R.id.progressBar);
+        pBar.setVisibility(View.GONE);
 
         Button button1 = (Button) findViewById(R.id.start_asynctask);
         button1.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +48,8 @@ public class MainActivity extends Activity {
 
         String URL;
         HashMap<String, String> parameters;
-        private ProgressDialog pDialog;
+
+        private TextView progressText;
 
         public DownloadStreamTask(String url, HashMap<String, String> params){
             this.URL = url;
@@ -65,50 +69,73 @@ public class MainActivity extends Activity {
                         String value = entry.getValue();
                         URL += "&"+key+"="+value;
                     }
-                    Log.d(TAG, URL);
+                    Log.d(TAG, "url="+URL);
                 }
+                Log.d(TAG, "reading result");
                 URL apiURL = new URL(URL);
+                Log.d(TAG, "objet url créé");
                 URLConnection urlConnection = apiURL.openConnection();
-                int lenghtOfFile = urlConnection.getContentLength();
-
+                Log.d(TAG, "urlconnection créé");
+                urlConnection.setDoOutput(true);
+                Log.d(TAG, "param output");
+                try {
+                    urlConnection.connect();
+                } catch (Exception e) {
+                    Log.d(TAG, "tentative de connection foirée");
+                    Log.d(TAG, e.toString());
+                }
+                Log.d(TAG, "tentative de connection");
                 //reading the response from the urlconnection via a bufferedreader
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                String line;
+                for (String line = bufferedReader.readLine(); line != null; line = bufferedReader.readLine()) {
+                    content.append(line + "\n");
+                    Log.d(TAG, "line");
+                }
+                /*
                 while ((line = bufferedReader.readLine()) != null)
                 {
                     content.append(line + "\n");
-                }
+                    Log.d(TAG, "line" + line);
+                }*/
+                Log.d(TAG, "fin input");
                 bufferedReader.close();
                 return content.toString();
             }
                 catch (Exception e) {}
-            return "";
+            return "tâche-finie-fail";
         }
 
         @Override
         protected void onPostExecute(String result)
         {
-            pDialog.dismiss();
-            TextView txt = (TextView) findViewById(R.id.output);
-            txt.setText("Response is: "+ result.substring(0,500));
-//
+            //pDialog.dismiss();
+            //progressText.setText("Response is: "+ result.substring(0,500));
+            pBar.setVisibility(View.GONE);
+
             try {
+                progressText.setText("Tâche finie et réussie");
                 JSONObject theObject = new JSONObject(result);
-                txt.setText("Response is: "+theObject.getString("status")+"\n"+
-                        theObject.getString("count")+"/"+theObject.getString("count_total"));
+                //txt.setText("Response is: "+theObject.getString("status")+"\n"+theObject.getString("count")+"/"+theObject.getString("count_total"));
             } catch (Exception e){
-                txt.setText("Error during process");
+                //txt.setText("Error during process");
+                progressText.setText("Tâche finie et ratée");
             }
+
             super.onPostExecute(result);
         }
 
         @Override
         protected void onPreExecute() {
+            pBar.setVisibility(View.VISIBLE);
+            progressText = (TextView) findViewById(R.id.progressText);
+            progressText.setText("Tâche en cours");
+
+            /*
             pDialog = new ProgressDialog(getApplicationContext());
             pDialog.setMessage("Reqûete en cours de traitement");
             pDialog.setIndeterminate(false);
             pDialog.setCancelable(false);
-            pDialog.show();
+            pDialog.show();*/
             super.onPreExecute();
         }
 
