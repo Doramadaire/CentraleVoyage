db5c0d78ec99c66b1bfb6343a73c24425d88fc6e
Claverie Guilhem <gclaverie@calade160.etu.ec-m.fr>
2017-01-31 19:09:11 +0100

    permission et gestion du GPS en cours

--- app/src/main/java/com/example/gclaverie/centralevoyage/MainActivity.java
+++ app/src/main/java/com/example/gclaverie/centralevoyage/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.gclaverie.centralevoyage;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -18,19 +24,43 @@ import android.util.Log;
 import android.widget.TextView;
 import org.json.JSONObject;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     protected ProgressBar pBar;
     protected Location location;
+    private TextView textError;
+    private static final int LOCATION_PERMISSION = 307;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textError = (TextView) findViewById(R.id.textError);
+        textError.setText("");
 
-        location = getLastKnownLocation();
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "si on a pas la permission on va la demander");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Log.d(TAG, "dans le if pour expliquer");
 
+            } else {
+                Log.d(TAG, "HORS du dans le if pour expliquer");
+                // No explanation needed, we can request the permission.
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+        } else {
+            Log.d(TAG, "pas besoin de demander la permission, on l'a déjà");
+        }
 
         final HashMap<String, String> parameters = new HashMap<String, String>();
         parameters.put("lat", "43.14554197717751");
@@ -47,6 +77,27 @@ public class MainActivity extends Activity {
                 new DownloadStreamTask(apiUrl, parameters).execute();
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "ouais permission accordée");
+
+                    // permission was granted, yay!
+
+                } else {
+                    Log.d(TAG, "permissions refusée");
+                    textError.setText("L'application a besoin des données de géolocalisation pour fonctionner");
+                    // permission denied, boo!
+                    // Disable the functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 
     private class DownloadStreamTask extends AsyncTask<String, Void, String> {
